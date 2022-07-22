257340e1bb9f4b82df86ac4cd12afd4fdf938f71
Hiago Petris <hiago.petris@gmail.com>
2022-07-22 08:30:31 -0300

    Chain of Responsability

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java-DesignPatterns
 Project developed as part of Alura - Java Projects Design and Architecture formation
+
+https://refactoring.guru/design-patterns
---
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesDescontos {
+    public static void main(String[] args) {
+        CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
+        Orcamento segundo = new Orcamento(new BigDecimal("1000"), 4);
+
+        System.out.println(calculadora.calcular(primeiro));
+        System.out.println(calculadora.calcular(segundo));
+    }
+}
