ca33c9ed274ff0e8a7e1f523979f85707db13e7c
Hiago Petris <hiago.petris@gmail.com>
2022-07-22 08:39:04 -0300

    Template Method

--- src/br/com/alura/loja/TestesDescontos.java
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -12,8 +12,10 @@ public class TestesDescontos {
         CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
         Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
         Orcamento segundo = new Orcamento(new BigDecimal("1000"), 4);
+        Orcamento terceiro = new Orcamento(new BigDecimal("200"), 4);
 
         System.out.println(calculadora.calcular(primeiro));
         System.out.println(calculadora.calcular(segundo));
+        System.out.println(calculadora.calcular(terceiro));
     }
 }
